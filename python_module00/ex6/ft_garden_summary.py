f89f46ff2563ed29b5d71737f09493a238c04744
Steven Francisco Jaramillo <stifrank97@gmail.com>
2026-02-02 13:41:11 +0100

    Module0 OK

---
+++ python_module00/ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+   garden_name = input("Enter a garden name: ")
+   plants = int(input("Enter number of plants: "))
+   print(f"Graden: {garden_name}")
+   print(f"Plants: {plants}")
+   print("Status: Growing well!")
